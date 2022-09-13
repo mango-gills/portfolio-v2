aa8061aaf5e3db6f83f2262f0d3a83e4504ed4e8
mango-gills <juzzgil21@gmail.com>
2022-09-13 18:33:00 +0800

    more layout fix - contact section

--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,7 @@ export const elementStyles = {
   logoScale:
     "h-10 cursor-pointer sm:h-14 xl:h-20 hover:scale-125 hover:duration-150 hover:ease-out md:h-10",
   inputStyle:
-    "w-full p-2 bg-transparent border-2 border-red-500 rounded-sm outline-transparent outline text-xs sm:text-sm xl:text-lg placeholder:font-bold font-light",
+    "w-full py-2 px-4 bg-transparent border-2 border-red-500 rounded-sm outline-transparent outline text-xs sm:text-sm xl:text-lg placeholder:font-bold font-light bg-[#1f1f1f]",
   projectCard:
     "grid w-full h-16 lg:h-32 bg-[#3D3D3D] border-2 border-[#515151] shadow-black rounded-md sm:h-20 place-items-center",
   projectCardHeading:
