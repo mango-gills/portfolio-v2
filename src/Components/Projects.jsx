21b1ee33ca1ec5be8466b96cdf10d730adecf80a
mango-gills <juzzgil21@gmail.com>
2022-09-13 18:54:56 +0800

    fix projects layout laptop size

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,15 +3,15 @@ import { elementStyles } from "../styles";
 
 const Projects = () => {
   return (
-    <div className="bg-[#161617] pt-[120px] lg:flex lg:flex-row-reverse lg:h-full lg:items-start lg:py-12">
-      <div className="pr-16 pl-4 py-4 space-y-6 text-white lg:w-[60%] lg:space-y-0">
+    <div className="bg-[#161617] pt-[120px] lg:flex lg:flex-row-reverse lg:h-full lg:items-start lg:py-4 xl:py-12">
+      <div className="pr-16 pl-4 py-4 space-y-6 text-white lg:w-[55%] xl:w-[60%] lg:space-y-0">
         <h1 className="text-2xl font-bold lg:hidden">PROJECTS</h1>
         <img className="" src="./p5-1.png" alt="" />
-        <h1 className="text-2xl font-bold text-red-500 lg:text-5xl lg:py-6">
+        <h1 className="text-2xl font-bold text-red-500 lg:text-3xl xl:text-5xl lg:py-2 xl:py-6">
           Chapp
         </h1>
-        <div className="flex flex-col w-full lg:flex-row lg:justify-between lg:pb-4">
-          <div className="flex lg:flex-col lg:text-2xl lg:space-y-2 space-x-2 lg:space-x-0">
+        <div className="flex flex-col w-full lg:flex-row lg:justify-between lg:pb-2 xl:pb-4">
+          <div className="flex lg:flex-col lg:text-lg xl:text-2xl xl:space-y-2 space-x-2 lg:space-x-0">
             <p>Tech Stack:</p>
             <div className="flex space-x-2">
               <p className="text-red-500">ReactJS</p>
@@ -19,7 +19,7 @@ const Projects = () => {
               <p className="text-red-500">Firebase</p>
             </div>
           </div>
-          <div className="flex lg:flex-col lg:text-2xl lg:space-y-2 space-x-2 lg:space-x-0">
+          <div className="flex lg:flex-col lg:text-lg xl:text-2xl xl:space-y-2 space-x-2 lg:space-x-0">
             <p>Links:</p>
             <div className="flex space-x-2">
               <p className="text-red-500">Demo</p>
@@ -28,7 +28,7 @@ const Projects = () => {
             </div>
           </div>
         </div>
-        <p className="text-sm lg:text-2xl lg:leading-10">
+        <p className="text-sm lg:text-base xl:leading-10 xl:text-2xl lg:leading-6 text-justify">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque
           cum ad et, maxime laborum odit molestiae repudiandae consequatur ut
           voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit
@@ -38,7 +38,7 @@ const Projects = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-3 gap-2 px-8 pt-4 pb-8 lg:grid-cols-1 lg:w-[20%] lg:gap-6">
+      <div className="grid grid-cols-3 gap-2 px-8 pt-4 pb-8 lg:grid-cols-1 lg:w-[20%] xl:gap-6 lg:gap-4">
         <div className={elementStyles.projectCard}>
           <div className="flex flex-col text-center sm:text-left">
             <h1 className={elementStyles.projectCardHeading}>Project - 001</h1>
--- src/styles.js
+++ src/styles.js
@@ -6,7 +6,7 @@ export const elementStyles = {
   inputStyle:
     "w-full py-2 px-4 bg-transparent border-2 border-red-500 rounded-sm outline-transparent outline text-xs sm:text-sm xl:text-lg placeholder:font-bold font-light bg-[#1f1f1f]",
   projectCard:
-    "grid w-full h-16 lg:h-32 bg-[#3D3D3D] border-2 border-[#515151] shadow-black rounded-md sm:h-20 place-items-center",
+    "grid w-full h-16 xl:h-32 bg-[#3D3D3D] border-2 border-[#515151] shadow-black rounded-md sm:h-20 place-items-center",
   projectCardHeading:
-    "mb-1 text-sm font-bold sm:text-xl lg:text-3xl text-[#FF3131]",
+    "mb-1 text-sm font-bold sm:text-xl lg:text-xl xl:text-3xl text-[#FF3131]",
 };
