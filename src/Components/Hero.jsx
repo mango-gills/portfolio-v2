74bd406f6ad3288e36bc6456fc9368aae1ac4336
mango-gills <juzzgil21@gmail.com>
2022-09-14 20:14:48 +0800

    adjust projects layout - hero mobile style

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         variants={onDisplayVariant}
         initial="hidden"
         animate="display"
-        className="font-bold sm:text-6xl md:text-8xl text-white text-[2.9rem] lg:text-[6rem] xl:text-[8rem] mb-2 lg:mb-4 xl:mb-6 tracking-wider text-shadow-sm lg:text-shadow-lg"
+        className="font-bold sm:text-6xl md:text-8xl text-white text-[2.6rem] lg:text-[6rem] xl:text-[8rem] mb-2 lg:mb-4 xl:mb-6 tracking-wider text-shadow-sm lg:text-shadow-lg"
       >
         Hello World!
       </motion.h1>
@@ -26,16 +26,16 @@ const Hero = () => {
         {/* <h2 className="text-3xl font-medium md:text-3xl sm:text-2xl xl:text-3xl">
           Hello there!
         </h2> */}
-        <div className="lg:w-[500px] xl:w-[700px] lg:leading-snug xl:leading-normal my-6 sm:text-lg md:text-xl xl:text-2xl">
+        <div className="w-[290px] lg:w-[500px] xl:w-[700px] lg:leading-snug xl:leading-normal my-6 sm:text-lg text-xs md:text-xl xl:text-2xl">
           <p>
           My name is Mark Encinares, aiming to be a Full Stack Web Developer, I make fast and responsive web pages.
           </p>
           
           <p className="mt-8">Let's build something great!</p>
         </div>
-        <p className="mt-14 xl:mt-20 text-base lg:text-xl xl:text-2xl">
-          <span className="font-semibold text-red-500 border-2 px-6 py-4 lg:px-8 border-vividRed-900 cursor-pointer hover:text-white hover:bg-vividRed-900 duration-300 transition-all ease-in ">
-            Contact me here
+        <p className="mt-14 xl:mt-20 text-xs lg:text-xl xl:text-2xl">
+          <span className="font-semibold text-red-500 border-2 px-4 py-2 sm:px-6 sm:py-4 lg:px-8 border-vividRed-900 cursor-pointer hover:text-white hover:bg-vividRed-900 duration-300 transition-all ease-in ">
+            Contact me
           </span>
         </p>
       </motion.div>
