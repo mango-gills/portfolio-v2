82453017e1079b2f71269c3c6d0caff9f7cdb544
mango-gills <juzzgil21@gmail.com>
2022-09-20 18:23:40 +0800

    replace data on project description on project card click

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { elementStyles } from "../styles";
 import { projects } from "../projectsData";
 
 const Projects = () => {
+  const [project, setProject] = useState(projects[0]);
+
+  const handleData = (data) => {
+    setProject(data);
+  };
+
   return (
     <div className="bg-[#161617] pt-[120px] lg:flex lg:flex-row-reverse h-full lg:min-h-screen lg:items-center lg:justify-end lg:py-0 lg:pl-16 xl:pl-28">
       <div className="px-8 py-4 space-y-6 text-white lg:w-[65%] xl:w-[60%] lg:space-y-0">
         <h1 className="text-2xl font-bold lg:hidden">PROJECTS</h1>
         <img className="" src="./p5-1.png" alt="" />
         <h1 className="text-2xl font-bold text-vividRed-900 lg:text-3xl xl:text-5xl lg:py-2 xl:py-6">
-          Chapp
+          {project?.title}
         </h1>
         <div className="flex flex-col w-full lg:flex-row lg:justify-between lg:pb-2 xl:pb-4">
           <div className="flex lg:flex-col lg:text-lg xl:text-2xl xl:space-y-2 space-x-2 lg:space-x-0">
             <p>Tech Stack:</p>
             <div className="flex space-x-2">
-              <p className="text-vividRed-900">ReactJS</p>
-              <p className="text-vividRed-900">|</p>
-              <p className="text-vividRed-900">Firebase</p>
+              {project?.stack.map((list, idx) => {
+                return (
+                  <p key={idx} className="text-vividRed-900">
+                    {list}
+                  </p>
+                );
+              })}
             </div>
           </div>
           <div className="flex lg:flex-col lg:text-lg xl:text-2xl xl:space-y-2 space-x-2 lg:space-x-0">
             <p>Links:</p>
             <div className="flex space-x-2">
-              <p className="text-vividRed-900">Demo</p>
+              <a
+                href={`https://www.${project?.links.demo}`}
+                target={"_blank"}
+                rel={"external"}
+                className="text-vividRed-900"
+              >
+                Demo
+              </a>
               <p className="text-vividRed-900">|</p>
-              <p className="text-vividRed-900">Code</p>
+              <a
+                href={`https://www.${project?.links.code}`}
+                target="_blank"
+                className="text-vividRed-900"
+              >
+                Code
+              </a>
             </div>
           </div>
         </div>
@@ -41,7 +64,11 @@ const Projects = () => {
 
       <ul className="grid grid-cols-3 gap-2 px-8 lg:px-4 lg:grid-cols-1 lg:w-[20%] xl:gap-6 lg:gap-3">
         {projects.map((data, index) => (
-          <li key={index} className={elementStyles.projectCard}>
+          <li
+            key={index}
+            className={elementStyles.projectCard}
+            onClick={() => handleData(data)}
+          >
             <div className="flex flex-col text-center sm:text-left">
               <h1 className={elementStyles.projectCardHeading}>{data.title}</h1>
 
--- src/projectsData.js
+++ src/projectsData.js
@@ -1,74 +1,74 @@
 export const projects = [
-    {
-        images : ["1","2"],
-        id: "001",
-        title: "JS Game",
-        stack: ['ReactJS', 'Firebase'],
-        links: {
-            demo: "demo",
-            code: "code"
-        },
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.'
-
+  {
+    images: ["1", "2"],
+    id: "001",
+    title: "JS Game",
+    stack: ["HTML", "CSS", "JavaScript"],
+    links: {
+      demo: "google.com/",
+      code: "github.com/mango-gills",
     },
-    {
-        images : ["1","2"],
-        id: "002",
-        title: "Shappy",
-        stack: ['ReactJS', 'Firebase'],
-        links: {
-            demo: "demo",
-            code: "code"
-        },
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.'
-
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.",
+  },
+  {
+    images: ["1", "2"],
+    id: "002",
+    title: "Shappy",
+    stack: ["ReactJS", "Tailwind", "FakeStoreAPI"],
+    links: {
+      demo: "google.com/",
+      code: "github.com/mango-gills",
     },
-    {
-        images : ["1","2"],
-        id: "003",
-        title: "foodStreet",
-        stack: ['ReactJS', 'Firebase'],
-        links: {
-            demo: "demo",
-            code: "code"
-        },
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.'
-
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.",
+  },
+  {
+    images: ["1", "2"],
+    id: "003",
+    title: "foodStreet",
+    stack: ["ReactJS", "NodeJS", "Express", "MongoDB"],
+    links: {
+      demo: "google.com/",
+      code: "github.com/mango-gills",
     },
-    {
-        images : ["1","2"],
-        id: "004",
-        title: "CHAPP",
-        stack: ['ReactJS', 'Firebase'],
-        links: {
-            demo: "demo",
-            code: "code"
-        },
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.'
-
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.",
+  },
+  {
+    images: ["1", "2"],
+    id: "004",
+    title: "CHAPP",
+    stack: ["ReactJS", "Firebase"],
+    links: {
+      demo: "google.com/",
+      code: "github.com/mango-gills",
     },
-    {
-        images : ["1","2"],
-        id: "005",
-        title: "React Todo",
-        stack: ['ReactJS', 'Firebase'],
-        links: {
-            demo: "demo",
-            code: "code"
-        },
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.'
-
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.",
+  },
+  {
+    images: ["1", "2"],
+    id: "005",
+    title: "React Todo",
+    stack: ["ReactJS", "Tailwind", "Firebase"],
+    links: {
+      demo: "google.com/",
+      code: "github.com/mango-gills",
     },
-    {
-        images : ["1","2"],
-        id: "006",
-        title: "Chapp",
-        stack: ['ReactJS', 'Firebase'],
-        links: {
-            demo: "demo",
-            code: "code"
-        },
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.'
-
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.",
+  },
+  {
+    images: ["1", "2"],
+    id: "006",
+    title: "Chapp",
+    stack: ["ReactJS", "Firebase"],
+    links: {
+      demo: "google.com/",
+      code: "github.com/mango-gills",
     },
-]
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non itaque cum ad et, maxime laborum odit molestiae repudiandae consequatur ut voluptatem fugiat? Numquam, veniam dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente consequatur corrupti.",
+  },
+];
