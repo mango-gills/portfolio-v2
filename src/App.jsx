641bf2fd299d6817dec7a6fcccafdf165fe6394e
mango_gills <juzzgil21@gmail.com>
2023-03-11 14:39:23 +0800

    remove unused routes and pages, update 404 page styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,9 @@ import {
 } from "react-router-dom";
 
 import { AnimatePresence } from "framer-motion";
-import ContactPage from "./Pages/ContactPage";
 import Home from "./Pages/Home";
 import PageNotFound from "./Pages/PageNotFound";
-import ProjectsPage from "./Pages/ProjectsPage";
 import React from "react";
-import SkillsAndTools from "./Pages/SkillsAndTools";
 
 const App = () => {
   return (
@@ -19,9 +16,6 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/skills" element={<SkillsAndTools />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
           <Route exact path="/home" element={<Navigate to="/" />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -3,9 +3,26 @@ import React from "react";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>Page not found</h1>
-      <Link to={"/"}>Return to home</Link>
+    <div className="grid h-screen w-full place-items-center bg-[#1f1f1f] text-white">
+      <div className="text-center">
+        <h1 className="text-lg text-yellow-500 md:text-xl lg:text-2xl">
+          (╯°□°）╯︵ ┻━┻
+        </h1>
+        <div className="my-4">
+          <h1 className="text-7xl font-extrabold md:text-8xl lg:text-9xl">
+            404
+          </h1>
+          <h2 className="text-lg text-gray-300 md:text-2xl lg:text-3xl">
+            Page Not Found
+          </h2>
+        </div>
+        <Link
+          className="text-base font-normal text-vividRed-900/90 underline  underline-offset-4 md:text-xl lg:text-2xl"
+          to={"/"}
+        >
+          Return to home
+        </Link>
+      </div>
     </div>
   );
 };
